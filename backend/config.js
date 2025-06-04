0ef2e0f34afeed4ab5f54b3f5e73003caaf84e95
Sameer <sameerahmad2429@gmail.com>
2025-06-04 15:52:29 +0530

    url change

--- backend/config.js
+++ backend/config.js
@@ -3,8 +3,7 @@ dotenv.config();
 
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
-const STRIPE_SECRET_KEY =
-  "sk_test_51RVybaLSTLrP6URpPol3f7W084XexImbOu2FUwbsmqNLEGQqRlsuUnrTnPxdk5Jq0o8LS8R2i6jWcGxnWDwS0rju00NY4wLbDm";
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
 export default {
   JWT_USER_PASSWORD,
